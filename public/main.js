b32c0815772e2cb21484b2d62819a768d2c97121
geekboyisonnow <kennethfcannon@gmail.com>
2020-11-05 19:18:54 -0500

    Updating ptag in the Dom

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const count = 42;
+
+const main = () => {
+  // write all our code here inside the main
+
+  // Find the p tag
+const ptag = document.querySelector('p');
+console.log(ptag);
+
+  // Change the p tag text into whatever is inside of the variable count
+  ptag.textContent = count;
+
+  // document.querySelector('h1').textContent += '?'
 }
+
+
+// function main() {
+//   if (document.querySelector('h1.hello-world')) {
+//     document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+//   }
+// }
 
 document.addEventListener('DOMContentLoaded', main)
